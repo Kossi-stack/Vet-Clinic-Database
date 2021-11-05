c062cdf49326708a45db18044827a18470fbec13
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-11-05 14:03:22 -0700

    Run queries

--- data.sql
+++ data.sql
@@ -11,7 +11,7 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17);
 
-INSERT INTO owners (full_name,age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
 
 INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
 
@@ -23,5 +23,3 @@ UPDATE animals SET owner_id=2 WHERE name  IN ('Pikachu','Gabumon');
 UPDATE animals SET owner_id=3 WHERE name IN ('Devimon','Plantmon');
 UPDATE animals SET owner_id=4 WHERE name IN ('Charmander','Squirtle','Blossom');
 UPDATE animals SET owner_id=5 WHERE name IN ('Angemon','Boarmon');
-
-UPDATE owners SET full_name='Melody Pond' WHERE id=4;
